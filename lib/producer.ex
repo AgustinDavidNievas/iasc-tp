d4038abff07f600bde8bd0900df56429e1e23c2e
matias <example@example>
2019-11-17 00:56:41 -0300

    update param name

--- lib/producer.ex
+++ lib/producer.ex
@@ -6,13 +6,17 @@ defmodule Producer do
     GenServer.start_link(__MODULE__, name: __MODULE__)
   end
 
-  def handle_call({:sync_notify, event, timeout}, _from, state) do
+  def init(arg) do
+    { :ok, arg }
+  end
+
+  def handle_call({:sync_notify, msj, timeout}, _from, state) do
     #TODO hacer privada a la funcion sync_notify, crear otra async y hacer pattern matching aca?
-    sync_notify(event, timeout)
+    sync_notify(msj, timeout)
     {:reply, :ok, state}
   end
 
-  def sync_notify(event, timeout \\ 5000) do
-    GenServer.call(ColaActiva, {:notify, event}, timeout)
+  def sync_notify(msj, timeout \\ 5000) do
+    GenServer.call(ColaActiva, {:notify, {self(), :calendar.local_time(),msj}}, timeout)
   end
 end
