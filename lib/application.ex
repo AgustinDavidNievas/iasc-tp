2166cf4ed3f2bdbc8b292c2f522ae4f235d8e8a4
agustindavidnievas <agustindavidnievas90@gmail.com>
2019-12-01 00:19:02 -0300

    Clean

--- lib/application.ex
+++ lib/application.ex
@@ -1,12 +1,6 @@
 defmodule Iasc_tp.Application do
   use Application
 
-  #{:ok, colaUnoPid} = ColaActivaDynamicSupervisor.start_child(:uno)
-  #{:ok, consumerUnoPid} = ConsumerDynamicSupervisor.start_child(:uno)
-  #{:ok, consumerDosPid} = ConsumerDynamicSupervisor.start_child(:dos)
-  #GenStage.sync_subscribe(consumerUnoPid, to: colaUnoPid)
-  #GenStage.sync_subscribe(consumerDosPid, to: colaUnoPid)
-  
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -21,7 +15,5 @@ defmodule Iasc_tp.Application do
     #El Iasc_tp.Supervisor seria el supervisor de supervisores
     opts = [strategy: :one_for_one, name: Iasc_tp.Supervisor]
     Supervisor.start_link(children, opts)
-    # ConsumerDynamicSupervisor.start_child(:uno)
-    # RegistroCola.start
   end
 end
