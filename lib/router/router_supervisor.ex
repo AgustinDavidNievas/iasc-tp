6d2706c426801c2019a7ee58305faa8f3c0e6481
unknown <muracafernando@gmail.com>
2019-12-01 01:30:05 -0300

    logging

--- lib/router/router_supervisor.ex
+++ lib/router/router_supervisor.ex
@@ -1,6 +1,8 @@
 defmodule RouterSupervisor do
   use Supervisor
 
+  require Logger
+
   def start_link(_args) do
     Supervisor.start_link(__MODULE__, :ok)
   end
@@ -9,7 +11,7 @@ defmodule RouterSupervisor do
     children = [
        worker(Router, [[name: {:global, GlobalRouter}]], restart: :transient)
     ]
-    IO.puts "Iniciando supervisor del router..."
+    Logger.info("Iniciando supervisor del router...")
     Supervisor.init(children, strategy: :one_for_one)
   end
 end
