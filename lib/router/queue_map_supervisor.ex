007751922bf66d27d281c2bc1bb529039639b058
etolaba <emi.tolaba95@gmail.com>
2019-12-02 00:05:50 -0300

    Refactor supervisores de router y registry de colas

--- lib/router/queue_map_supervisor.ex
+++ lib/router/queue_map_supervisor.ex
@@ -3,15 +3,24 @@ defmodule RegistroColaSupervisor do
 
   require Logger
 
-  def start_link(_args) do
-    Supervisor.start_link(__MODULE__, :ok)
+  def start_link do
+    Supervisor.start_link(__MODULE__, name: __MODULE__)
   end
 
-  def init(:ok) do
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  def init(_init_arg) do
     Logger.info("Iniciando supervisor del mapa de colas...")
     children = [
-       worker(RegistroCola, [])
+      worker(RegistroCola, [])
     ]
     Supervisor.init(children, strategy: :one_for_one)
   end
+
+  def children do
+    Supervisor.which_children(__MODULE__)
+  end
+
 end
