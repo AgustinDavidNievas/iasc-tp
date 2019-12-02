18bf5457254e3eebc4a7895a436a798797f2f4da
mmiguenz <miguenz.matias@gmail.com>
2019-12-02 01:30:55 -0300

    limpio .iex

--- .iex.exs
+++ .iex.exs
@@ -1,9 +1,6 @@
  {:ok, pid_cola_pasiva1} =  Agent.start_link(fn -> {:queue.new, 0} end, name: :agent1)
  {:ok, pid_cola_pasiva2} =  Agent.start_link(fn -> {:queue.new, 0} end, name: :agent2)
 
- ColaPasiva.get_state(pid_cola_pasiva1)
-
-
 {:ok, colaUnoPid} = ColaActivaDynamicSupervisor.start_child(:cola_uno, pid_cola_pasiva1)
 {:ok, colaDosPid} = ColaActivaDynamicSupervisor.start_child(:cola_dos, pid_cola_pasiva2)
 
@@ -16,6 +13,4 @@ GenStage.sync_subscribe(consumerDosPid, to: colaUnoPid)
 
 Router.agregar_cola("uno", colaUnoPid)
 Router.agregar_cola("dos", colaDosPid)
-
-ColaPasiva.get_state(pid_cola_pasiva1)
 
