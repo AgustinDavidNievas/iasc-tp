8749ece8a4a60006c52ef4c417bf138d3b5dd81a
mmiguenz <miguenz.matias@gmail.com>
2019-12-03 02:29:07 -0300

    implentacion replicacion

--- lib/colaActiva.ex
+++ lib/colaActiva.ex
@@ -1,23 +1,28 @@
 defmodule ColaActiva do
   use GenStage
 
-  def child_spec({id, dispatcher}) do
+  def child_spec({id, dispatcher, pid_cola_pasiva}) do
     name = Module.concat(__MODULE__, id)
-    %{id: name, start: {__MODULE__, :start_link, [name, dispatcher]}, type: :worker}
+    %{id: name, start: {__MODULE__, :start_link, [name, dispatcher, pid_cola_pasiva]}, type: :worker}
   end
 
-  def start_link(name, dispatcher) do
-    GenStage.start_link(__MODULE__, dispatcher, name: name)
+  def start_link(name, dispatcher, pid_cola_pasiva) do
+    ColaActivaReg.registrar_cola_pasiva(pid_cola_pasiva)
+    GenStage.start_link(__MODULE__, {dispatcher, pid_cola_pasiva}, name: name)
   end
 
-  def init(dispatcher) do
+  def init({dispatcher, pid_cola_pasiva}) do
     #GenStage.DemandDispatcher envia el mensaje a un solo consumer (usando fifo)
     #GenStage.BroadcastDispatcher envisa el mensaje a todos los consumer
-    {:producer, {:queue.new, 0}, dispatcher: dispatcher}
+    {:producer, ColaPasiva.get_state(pid_cola_pasiva), dispatcher: dispatcher}
   end
 
   def handle_call({:notify, event}, from, {queue, pending_demand}) do
     queue = :queue.in({from, event}, queue)
+
+    [{_ , cola_pasiva_agent}] = ColaActivaReg.get_cola_pasiva()
+    ColaPasiva.insert(cola_pasiva_agent, {from, event}, pending_demand)
+
     dispatch_events(queue, pending_demand, [])
   end
 
@@ -32,6 +37,10 @@ defmodule ColaActiva do
   defp dispatch_events(queue, demand, events) do
     case :queue.out(queue) do
       {{:value, {from, event}}, queue} ->
+
+        [{_ , cola_pasiva_agent}] = ColaActivaReg.get_cola_pasiva()
+        ColaPasiva.remove(cola_pasiva_agent, {from, event}, demand)
+
         GenStage.reply(from, :ok)
         dispatch_events(queue, demand - 1, [event | events])
       {:empty, queue} ->
